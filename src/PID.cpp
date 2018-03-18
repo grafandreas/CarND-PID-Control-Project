776899a4c93587a1b9a34c50a9a57191b437a29e
Andreas Graf <ag-online@quanxinquanyi.de>
2018-03-18 11:09:30 +0100

    My solution

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,28 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    this->d_error = 0.0;
+    this->p_error = 0.0;
+    this->i_error = 0.0;
+
+    this->prev_cte = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - prev_cte;
+    i_error += cte;
+    p_error = cte;
+
+    prev_cte = cte;
+
+
 }
 
 double PID::TotalError() {
+   return  Kp * p_error + Ki*i_error + Kd*d_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,9 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+
+  // Tweaking inspired by algorithm on Wikipedia: Manual tuning
+  pid.Init(0.14,0.0,2.3);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -58,6 +60,8 @@ int main()
           * another PID controller to control the speed!
           */
           
+          pid.UpdateError(cte);
+          steer_value = -pid.TotalError();
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
